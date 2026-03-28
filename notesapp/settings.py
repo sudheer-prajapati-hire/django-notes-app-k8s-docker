fb59e659ca80c233dbbec37cbbb2890b96e77811
Ubuntu <ubuntu@ip-172-31-28-90.eu-north-1.compute.internal>
2026-03-28 18:03:23 +0000

    Updated .gitignore

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -90,12 +90,9 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        
     }
 }
 
